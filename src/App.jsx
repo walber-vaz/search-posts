0baba6778c4fd91c62e399d9cd296017ce49a7ee
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-11-10 08:50:31 -0300

    feat: add counter and update posts in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,55 @@ import { Component } from 'react';
 
 export default class App extends Component {
   state = {
-    posts: [],
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'Lorem ipsum dolor sit amet.',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
+      },
+      {
+        id: 2,
+        title: 'Lorem ipsum dolor sit amet.',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
+      },
+      {
+        id: 3,
+        title: 'Lorem ipsum dolor sit amet.',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
+      },
+      {
+        id: 4,
+        title: 'Lorem ipsum dolor sit amet.',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
+      },
+    ],
   };
+  timer = null;
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'Lorem ipsum dolor sit amet.',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
-          },
-          {
-            id: 2,
-            title: 'Lorem ipsum dolor sit amet.',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
-          },
-          {
-            id: 3,
-            title: 'Lorem ipsum dolor sit amet.',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
-          },
-          {
-            id: 4,
-            title: 'Lorem ipsum dolor sit amet.',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
-          },
-        ],
-      });
-    }, 5000);
+    this.handleTimeout();
   }
 
-  render() {
-    const { posts } = this.state;
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
 
-    if (!posts.length) {
-      return <h1>Loading...</h1>;
-    }
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'Ola mundo';
+
+    this.timer = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 2000);
+  };
+
+  render() {
+    const { posts, counter } = this.state;
 
     return (
       <section>
@@ -49,6 +60,8 @@ export default class App extends Component {
             <p>{post.body}</p>
           </article>
         ))}
+        <hr />
+        <h1>{counter}</h1>
       </section>
     );
   }
